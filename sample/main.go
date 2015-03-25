a6c8280776282cbdef1273a9307032fb607f6fdc
mogeta <muzigen@gmail.com>
2015-03-25 11:01:35 +0900

    sample/main.go read token from config.yaml

--- sample/main.go
+++ sample/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "github.com/mogeta/goclack"
+import (
+	"io/ioutil"
+
+	"github.com/mogeta/goclack"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	configFile = "config.yaml"
+)
+
+type config struct {
+	Token string
+}
 
 func main() {
-	bot := goclack.New("your bot token")
+
+	//Load token from yaml
+	buf, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return
+	}
+
+	var m config
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	//run bot
+	bot := goclack.New(m.Token)
 	bot.Run()
 }
